1954be68d3a78ecebd30f838871489c656904a3c
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2023-09-22 12:01:25 -0300

    Getters e Setters automáticos
    Necessário instalar a extensão PHP 8 Getters e Setters no VSCode.

--- index.php
+++ index.php
@@ -4,6 +4,13 @@ $livro = new Livro;
 $livro->setAutor("J.R.R. Tolkien");
 $livro->setTitulo("O Hobbit");
 $livro->setPaginas(300);
+
+$outroLivro = new Livro;
+// $outroLivro->setTitulo("HTML5");
+// $outroLivro->setAutor("Maujor");
+
+// Métodos com return $this/self permitem ENCADEAMENTO nas chamadas
+$outroLivro->setTitulo("HTML5")->setAutor("Maujor");
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -13,6 +20,8 @@ $livro->setPaginas(300);
     <title>Exercício 01 (corrigido)</title>
 </head>
 <body>
+    <pre> <?=var_dump($outroLivro)?></pre>
+
     <h1>Exercício 01 (corrigido)</h1>
     <hr>
     <ul>
--- src/Livro.php
+++ src/Livro.php
@@ -4,4 +4,71 @@ class Livro {
     private string $autor;
     private int $paginas = 0;
    
+
+    public function getTitulo(): string
+    {
+        return $this->titulo;
+    }
+
+    /**
+     * Set the value of titulo
+     *
+     * @param string $titulo
+     *
+     * @return self
+     */
+    public function setTitulo(string $titulo): self
+    {
+        $this->titulo = $titulo;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of autor
+     *
+     * @return string
+     */
+    public function getAutor(): string
+    {
+        return $this->autor;
+    }
+
+    /**
+     * Set the value of autor
+     *
+     * @param string $autor
+     *
+     * @return self
+     */
+    public function setAutor(string $autor): self
+    {
+        $this->autor = $autor;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of paginas
+     *
+     * @return int
+     */
+    public function getPaginas(): int
+    {
+        return $this->paginas;
+    }
+
+    /**
+     * Set the value of paginas
+     *
+     * @param int $paginas
+     *
+     * @return self
+     */
+    public function setPaginas(int $paginas): self
+    {
+        $this->paginas = $paginas;
+
+        return $this;
+    }
 }
