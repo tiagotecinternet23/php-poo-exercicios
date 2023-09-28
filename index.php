68fce5947aab7cc095d00fbb2e332812c9bf1f60
Tiago B. dos Santos <tiago.tecinternet23@gmail.com>
2023-09-28 08:46:41 -0300

    Update index.php

--- index.php
+++ index.php
@@ -10,15 +10,11 @@ $livro->setPaginas(300);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercício 01 (corrigido)</title>
+    <title>Exercício 02 (corrigido)</title>
 </head>
 <body>
-    <h1>Exercício 01 (corrigido)</h1>
+    <h1>Exercício 02 (corrigido)</h1>
     <hr>
-    <ul>
-        <li>Autor: <?=$livro->getAutor()?></li>
-        <li>Livro: <?=$livro->getTitulo()?></li>
-        <li>Páginas: <?=$livro->getPaginas()?></li>
-    </ul>
+    
 </body>
 </html>
